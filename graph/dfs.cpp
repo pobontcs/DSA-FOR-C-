080735a09cf0947f27f96ec42b0f0b74775dded2
Abdullah Al Hossain <127250036+pobontcs@users.noreply.github.com>
2024-09-19 12:43:16 +0600

    Update dfs.cpp

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -1,10 +1,31 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-#include<vector>  
+#include<bits/stdc++.h> 
 using namespace std;
+
+void dfs(char c,map<char,vector<char>>&adj,map<char,bool>&visited){
+
+        visited[c]=true;
+        cout<<c<<" ";
+
+            for(char neighbour:adj[c]){
+                if(!visited[neighbour]){
+                    dfs(neighbour,adj,visited);
+                }
+            }
+
+}
+
 
 int main()
 {
+        map<char, vector<char>> adj;
+        map<char,bool>visited;
 
+    // Adding edges (Directed graph example)
+    adj['A'].push_back('B');
+    adj['A'].push_back('C');
+    adj['B'].push_back('D');
+    adj['C'].push_back('D');
+    adj['D'].push_back('E');
+
+    dfs('A',adj,visited);
 }
